247f25fdd84281522b54bc3cea394f5c046c0da9
Anton Kahn <freelead@gmail.com>
2021-04-28 12:13:46 -0600

    Added jest

---
+++ js/tests/sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('../sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 1.15 + 2.3 to equal 3.45', () => {
+  expect(sum(1.15, 2.3)).toBe(3.45);
+});
