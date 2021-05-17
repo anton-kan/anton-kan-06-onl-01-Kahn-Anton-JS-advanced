923901b700305f5ce00694859e450ad86526e13f
Anton Kahn <freelead@gmail.com>
2021-05-16 21:17:07 -0600

    Implemented math actions output in DOM using Browserify

---
+++ js/math.js
@@ -0,0 +1,39 @@
+const sum = require('./sum');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+const discriminant = require('./discriminant');
+
+const aInputElement = document.querySelector('#a-input');
+const bInputElement = document.querySelector('#b-input');
+const cInputElement = document.querySelector('#c-input');
+const actionSelectElement = document.querySelector('#action-select');
+const calculateBtnElement = document.querySelector('#calculate-btn');
+const resultInputElement = document.querySelector('#result-input');
+
+calculateBtnElement.addEventListener('click', calculate);
+
+function calculate() {
+  const a = +aInputElement.value;
+  const b = +bInputElement.value;
+  const c = +cInputElement.value;
+  let result;
+  switch (actionSelectElement.value) {
+    case 'sum':
+      result = sum(a, b);
+      break;
+    case 'subtract':
+      result = subtract(a, b);
+      break;
+    case 'multiply':
+      result = multiply(a, b);
+      break;
+    case 'divide':
+      result = divide(a, b);
+      break;
+    case 'discriminant':
+      result = discriminant(a, b, c);
+      break;
+  }
+  resultInputElement.value = result;
+}
