64af352aca31291ea5c56c33130379d381137c22
Anton Kahn <freelead@gmail.com>
2021-05-26 15:58:52 -0600

    Corrected the comments from the code review

--- src/render-list/render-list.js
+++ src/render-list/render-list.js
@@ -1,5 +1,11 @@
 import renderItem from './render-item/render-item';
 
-const renderList = (array, tooltip) => array.map((item) => renderItem(item, tooltip)).join('');
+const renderList = (array, tooltip) => {
+  const listElement = array.reduce((list, item) => {
+    list.append(renderItem(item, tooltip));
+    return list;
+  }, document.createElement('ul'));
+  return listElement;
+};
 
 export default renderList;
