9984800b2b340a5acc140da67de92a017fcec782
Anton Kahn <freelead@gmail.com>
2021-06-06 21:53:23 -0600

    Created boilerplate React app

---
+++ react-array-methods/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main>
+        <div className="App-container"></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
