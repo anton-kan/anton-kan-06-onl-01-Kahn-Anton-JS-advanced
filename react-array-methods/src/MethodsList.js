9a321cf643f9aaea989f09bba132ba5a6e23c5fb
Anton Kahn <freelead@gmail.com>
2021-06-07 22:44:25 -0600

    Implemented filters

--- react-array-methods/src/MethodsList.js
+++ react-array-methods/src/MethodsList.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MethodItem from './MethodItem';
 
 const MethodsList = ({ title, methods }) => {
+  const [filter, setFilter] = useState('');
+  const [filteredMethods, setFilteredMethods] = useState([]);
+
+  const changeFilter = (event) => {
+    const newFilter = event?.target?.value;
+    setFilter(newFilter);
+    setFilteredMethods(methods.filter((method) => method.toLowerCase().includes(newFilter.toLowerCase())));
+  };
+
+  const list = filter ? filteredMethods : methods;
   return (
     <div className="MethodsList">
       <h2>{title}</h2>
+      <input type="text" placeholder="Start typing to filter..." value={filter} onChange={changeFilter} />
       <ul>
-        {methods.map((method) => (
+        {list.map((method) => (
           <MethodItem key={method} method={method} />
         ))}
       </ul>
--- react-array-methods/src/MethodsListsContainer.js
+++ react-array-methods/src/MethodsListsContainer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
 import MethodsList from './MethodsList';
 import Tooltip from './Tooltip';
 
