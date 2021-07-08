7d9ff508a255956f5ef4bbaef4353ccd2bbbc467
Anton Kahn <freelead@gmail.com>
2021-07-08 00:01:08 -0600

    Fixed tests

--- react-array-methods/src/components/MethodsListsContainer/index.test.js
+++ react-array-methods/src/components/MethodsListsContainer/index.test.js
@@ -7,13 +7,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 test('renders tooltip and 3 lists', () => {
   const lists = [
-    { key: 'first', title: 'Title 1' },
-    { key: 'second', title: 'Title 2' },
-    { key: 'third', title: 'Title 3' },
+    { key: 'first', title: 'Title 1', list: ['item 11', 'item 12', 'item 13'] },
+    { key: 'second', title: 'Title 2', list: ['item 21'] },
+    { key: 'third', title: 'Title 3', list: ['item 31', 'item 32'] },
   ];
-  const listsState = { first: ['item 11', 'item 12', 'item 13'], second: ['item 21'], third: ['item 31', 'item 32'] };
 
-  const mockListsReducer = (state, action) => state || listsState;
+  const mockListsReducer = (state, action) => state || [];
   const reducer = { lists: mockListsReducer };
   const store = configureStore({ reducer });
 
