4e1e38f1e99f244d3e3481f7f0600aaa160a0fd7
Anton Kahn <freelead@gmail.com>
2021-07-10 21:56:12 -0600

    Added redirect to Home for production environment

--- react-array-methods/src/helpers/routing_paths.ts
+++ react-array-methods/src/helpers/routing_paths.ts
@@ -4,6 +4,7 @@ const ROUTING_PATHS = {
   solution: '/solution',
   reference: '/reference',
   method: '/reference/:methodName',
+  prodStartingPoint: '/anton-kan-06-onl-01-Kahn-Anton-JS-advanced/react-array-methods/build/',
 };
 
 export default ROUTING_PATHS;
--- react-array-methods/src/pages/Main.tsx
+++ react-array-methods/src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import ROUTING_PATHS from '../helpers/routing_paths';
 import Home from './Home';
@@ -13,6 +13,9 @@ const Main = () => {
     <main>
       <div className="App-container">
         <Switch>
+          <Route path={ROUTING_PATHS.prodStartingPoint}>
+            <Redirect to={ROUTING_PATHS.home} />
+          </Route>
           <Route exact path={ROUTING_PATHS.home} component={Home} />
           <Route path={ROUTING_PATHS.game} component={Game} />
           <Route path={ROUTING_PATHS.solution} component={Solution} />
