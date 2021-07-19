c8a4f4ecbd8354f5a8bdfdc09dfac38ef3484a06
Anton Kahn <freelead@gmail.com>
2021-07-18 22:38:15 -0600

    Implemented winning indicator

--- react-array-methods/src/pages/Game.tsx
+++ react-array-methods/src/pages/Game.tsx
@@ -3,6 +3,7 @@ import Counter from '../components/Counter';
 import { useSelector } from 'react-redux';
 import IState from '../interfaces/IState';
 import IMethodsList from '../interfaces/IMethodsList';
+import checkMethodLists from '../controller/check-method-lists';
 
 const Game = () => {
   const lists: IMethodsList[] = [
@@ -13,10 +14,12 @@ const Game = () => {
 
   const listsState = useSelector((state: IState) => state.lists);
   lists.forEach((item) => (item.list = listsState[item.key] || []));
+  const won = checkMethodLists(listsState);
 
   return (
     <section>
       <h2>The game has started!</h2>
+      {won && <div>You Won!</div>}
       <Counter />
       <MethodsListsContainer readonly={false} lists={lists} withLinks={false} />
     </section>
